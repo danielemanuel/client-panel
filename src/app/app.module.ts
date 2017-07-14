89fac8e45181e253234d5d981fee75141aa31955
danielemanuel <daniel.costea@vkernel.ro>
2017-07-14 14:38:23 +0100

    * client module created * client module dependency injection in client service * clinet component is able to fetch data from the firebase DB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
+//Angular fire imports
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
 
+//Component imports
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ClientsComponent } from './components/clients/clients.component';
@@ -14,12 +19,22 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+//Service imports
+import { ClientService } from './services/client.service';
 
 const appRoutes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent }
 ];
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAcj2qUtJrStGeqep1PX2jIaMFhqxzJsF8",
+  authDomain: "clientpanel-f1fa0.firebaseapp.com",
+  databaseURL: "https://clientpanel-f1fa0.firebaseio.com",
+  storageBucket: "clientpanel-f1fa0.appspot.com",
+  messagingSenderId: "123576774266"
+}
 
 @NgModule({
   declarations: [
@@ -38,9 +53,14 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth,
+    AngularFireDatabase,
+    ClientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
